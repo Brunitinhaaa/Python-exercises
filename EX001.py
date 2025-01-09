5034523465151f2fe3081805d55dde8a9d270e8e
Bruna Fraga <brunafraga12@gmail.com>
2025-01-09 20:24:05 -0300

    changes in exercises

--- EX001.py
+++ EX001.py
@@ -1,3 +1,33 @@
-programmingVariable = 'Hello dear world!'
+programmingVariable = '''
+╔════════════════════════════════════════════════════════════════════════╗
+║ 🌟                                                                   🌟║
+║                          🧙‍♂️ O Zen of Python 🧙‍♀️                       ║
+║ 🌟                                                                   🌟║
+╠════════════════════════════════════════════════════════════════════════╣
+║                                                                        ║
+║ 📜 Beautiful is better than ugly.                                      ║
+║ 📜 Explicit is better than implicit.                                   ║
+║ 📜 Simple is better than complex.                                      ║
+║ 📜 Complex is better than complicated.                                 ║
+║ 📜 Flat is better than nested.                                         ║
+║ 📜 Sparse is better than dense.                                        ║
+║ 📜 Readability counts.                                                 ║
+║ 📜 Special cases aren't special enough to break the rules.             ║
+║ 📜 Although practicality beats purity.                                 ║
+║ 📜 Errors should never pass silently.                                  ║
+║ 📜 Unless explicitly silenced.                                         ║
+║ 📜 In the face of ambiguity, refuse the temptation to guess.           ║
+║ 📜 There should be one-- and preferably only one --obvious way to do   ║
+║     it.                                                                ║
+║ 📜 Although that way may not be obvious at first unless you're Dutch.  ║
+║ 📜 Now is better than never.                                           ║
+║ 📜 Although never is often better than *right* now.                    ║
+║ 📜 If the implementation is hard to explain, it's a bad idea.          ║
+║ 📜 If the implementation is easy to explain, it may be a good idea.    ║
+║ 📜 Namespaces are one honking great idea -- let's do more of those!    ║
+║                                                                        ║
+╚════════════════════════════════════════════════════════════════════════╝
+'''
 
 print(programmingVariable)
+
--- EX002.py
+++ EX002.py
@@ -1,3 +1,9 @@
+"""
 name = input('whats your name? ')
-
 print('Hello',name,'! Welcome to our first program!')
+"""
+
+#FORMAT
+
+nome = input('Digite o nome')
+print(f'Nome recebido usando format: {nome}')
